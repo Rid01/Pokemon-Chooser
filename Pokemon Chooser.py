cb099a8de91aba48ef156c62436c4f2f6c8a82b2
Rid01 <ridwansphone@gmail.com>
2019-05-12 17:15:56 +0100

    Add files via upload

---
+++ Pokemon Chooser.py
@@ -0,0 +1,20 @@
+import random
+
+#list containing all the variables
+list = {
+    'Pokemon Red',
+    'Pokemon Blue',
+    'Pokemon Yellow',
+    'Pokemon Gold',
+    'Pokemon Silver',
+    'Pokemon Crystal',
+    'Pokemon Sapphire',
+    'Pokemon Ruby',
+    'Pokemon Emerald',
+    'Pokemon FireRed',
+    'Pokemon LeafGreen'
+    }
+#prints the randomly selected variable
+print(random.sample(list, 1)) 
+    
+    
